d585b8cd0e795c445bf8e7b90c1723bc235595e4
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2015-07-22 21:48:38 -0700

    adding first challenge.

---
+++ 07/22.py
@@ -0,0 +1,8 @@
+def list_all_keys_in_dict(my_dict):
+    """ print all keys in the dictionary """
+    pass
+
+
+
+assert list_all_keys_in_dict({"a": "b"}) == ["a"]
+assert list_all_keys_in_dict({"a": "b", "c": "d"}) == ["a", "c"]
