9a19fc6fa2359f3eae701b985cbdeb9f01dec88e
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2015-07-22 22:43:52 -0700

    adding 21.py

---
+++ 07/21.py
@@ -0,0 +1,9 @@
+def size_of_dictionary(my_dict):
+    """
+    return an integer representing the number of items in the
+    dictionary.
+    """
+
+
+assert size_of_dictionary({"a":"b"}) == 1
+assert size_of_dictionary({"a":"b", "c": "D"}) == 2
