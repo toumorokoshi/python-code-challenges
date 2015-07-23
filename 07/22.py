be321aecca6be26c1b3770ade4e558b34b20a61d
Koji <kojitsutsumi@gmail.com>
2015-07-22 22:32:32 -0700

    I solved this puzzle

--- 07/22.py
+++ 07/22.py
@@ -1,8 +1,10 @@
 def list_all_keys_in_dict(my_dict):
     """ print all keys in the dictionary """
-    pass
-
-
+    list = []
+    for x in my_dict:
+        print (x)
+        list.append(x)
+    return list
 
 assert list_all_keys_in_dict({"a": "b"}) == ["a"]
 assert list_all_keys_in_dict({"a": "b", "c": "d"}) == ["a", "c"]
