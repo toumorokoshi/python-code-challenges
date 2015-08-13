e1333f67347f568d0c09b87a94b9ff509a132bb4
Koji <kojitsutsumi@gmail.com>
2015-08-12 22:20:12 -0700

    solved 8/12/15

--- 07/27.py
+++ 07/27.py
@@ -5,14 +5,23 @@ def lotto_winner(lotto_tickets_by_person, winning_number):
     list of the lotto tickets the person has, returning the
     name of the person who has the winning number.
     """
+    z=[]
+    for k, v in lotto_tickets_by_person.iteritems():
 
+        for x in v:
 
-
+            if x == winning_number:
+                print x
+                return k
 lotto_numbers = {
     "john": [15, 17, 12, 13],
     "mark": [1, 10, 19, 14],
     "tracy": [3, 4, 5]
 }
+
+
+
+
 
 assert lotto_winner(lotto_numbers, 1) == "mark"
 assert lotto_winner(lotto_numbers, 4) == "tracy"
